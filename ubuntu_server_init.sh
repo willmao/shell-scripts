38ddfbee34edea09a4bf5c8d6e3b791ccccf7b2a
williams <794081758@qq.com>
2017-10-01 18:09:30 +0800

    add ubuntu server init script

---
+++ ubuntu_server_init.sh
@@ -0,0 +1,40 @@
+# -------------------------------------------------------------------------------------------
+# This script aims to help to init ubuntu server 17.04 with the following features enabled:
+#   tcp fastopen
+#   google bbr
+# and install the following softwares:
+#   docker-engine
+#   docker-compose
+# -------------------------------------------------------------------------------------------
+
+#!/bi/bash
+
+# enable tcp fastopen feature
+if !(grep 'net.ipv4.tcp_fastopen=3' /etc/sysctl.conf > /dev/null)
+then
+  echo 3 > /proc/sys/net/ipv4/tcp_fastopen
+  echo 'net.ipv4.tcp_fastopen=3' >> /etc/sysctl.conf
+fi
+
+# enable google bbr
+if !(grep 'net.core.default_qdisc=fq' /etc/sysctl.conf > /dev/null)
+then
+  echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+fi
+
+if !(grep 'net.ipv4.tcp_congestion_control=bbr' /etc/sysctl.conf > /dev/null)
+then
+  echo 'net.ipv4.tcp_congestion_control=bbr' >> /etc/sysctl.conf
+fi
+
+# install docker-engine
+if !(docker -v > /dev/null)
+then
+  curl -sSL https://get.daocloud.io/docker | sh # use script from daocloud to install docker-engine
+fi
+
+if !(docker-compose -v > /dev/null)
+then
+  curl -L https://get.daocloud.io/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose | sh # use script from daocloud to install docker-engine
+  chmod +x /usr/local/bin/docker-compose
+fi
